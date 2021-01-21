cb2db8344b80ec9283a0c9fed848b55bea7359b2
BGASM <slatte26@msu.edu>
2021-01-21 18:30:06 -0500

    Merge branch 'main' of https://git.heroku.com/msu-quiz-stage into main

--- msu_quiz/__init__.py
+++ msu_quiz/__init__.py
@@ -23,6 +23,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
 
     from .models.models import User, Quiz, MCQ, Question
     migrate.init_app(app, db)
